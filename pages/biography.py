4298825ac65ee7416daefc3b880cd4b50cc1e038
Manitra RANAIVOHARISON <manitra.harison@gmail.com>
2020-01-23 21:59:41 +0100

    Update biography.py

--- pages/biography.py
+++ pages/biography.py
@@ -21,7 +21,7 @@ def content():
     generate_data = st.button("Fetch Data From Wikipedia")
 
     file_name_source = str("data/" + name_list[name_list['Label'] == option]['Filename'].values[0])
-    file_name_destination = str(data/biographie" + option.replace(" ", "") + ".txt")
+    file_name_destination = str("data/biographie" + option.replace(" ", "") + ".txt")
     model_name = option.replace(" ", "_")
 
     if generate_data:
